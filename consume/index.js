9f95d20fc2e6553b6f6fc3961f378aabc9538037
XadillaX <admin@xcoder.in>
2017-02-27 11:05:17 +0800

    fix: make it connectable

---
+++ consume/index.js
@@ -0,0 +1,8 @@
+/**
+ * XadillaX <i@2333.moe> created at 2017-02-27 11:03:08 with ❤
+ *
+ * Copyright (c) 2017 xcoder.in, all rights reserved.
+ */
+'use strict';
+
+
--- server/boot/publisher.js
+++ server/boot/publisher.js
@@ -2,6 +2,7 @@
 
 module.exports = function(app, callback) {
     const publisher = require('../../lib/rabbitmq/pub');
+    console.log('Connecting to RabbitMQ...');
     publisher.start(function(err) {
         if(err) return callback(err);
         console.log('🐇 MQ producer started.');
