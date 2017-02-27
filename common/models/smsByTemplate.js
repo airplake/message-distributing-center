38523bf977a0d8a2107576e188e75dad389abaa2
X.C <119136016@qq.com>
2017-02-27 18:14:55 +0800

    添加普通发送短信接口

---
+++ common/models/smsByTemplate.js
@@ -0,0 +1,18 @@
+'use strict';
+var app = require('../../server/server');
+module.exports = function (sms) {
+    sms.beforeRemote('create', function (context, user, next) {
+        let req = context.req;
+        let smsByTemplate = app.get('smsByTemplate');
+        let content = smsByTemplate[req.body.templateid].replace('{0}',req.body.message);
+        req.body.message = content;
+        amqpConnection.publish('SmsExchange', 'sms', JSON.stringify({
+            tel: req.body.tel,//电话
+            content: content  //模板参数
+        }), 'direct');
+        next();
+    });
+};
+
+
+
