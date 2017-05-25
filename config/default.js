3bc6cd339ff02dcc423b0e4f33cc9130a4345233
503945930 <503945930@qq.com>
2017-05-25 01:24:48 -0400

    init

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ module.exports = {
 
   queue: {
        // connection: 'amqp://yedian:yedian123outfox@staging-api.chinacloudapp.cn:5672',
-    connection: 'amqp://admin:admin@192.168.1.10:5672',
+    connection: 'amqp://user:pwd@127.0.0.1:5672',
     channel: 'MDC_QUEUE',
     consumerAdapters: [{
       queueName: 'email',
@@ -10,7 +10,7 @@ module.exports = {
     }, {
       queueName: 'wechat',
       require: 'mdc-weixin',
-      tokenUrl: 'http://10.0.0.27:4000/internal/user/wechat/accessToken'
+      tokenUrl: 'http://127.0.0.1/getAccessToken'
     }]
   }
 }
