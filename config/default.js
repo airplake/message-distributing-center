b8008817387d2bd807a31ff161b139a9fb9cd02e
503945930 <503945930@qq.com>
2017-06-01 23:54:01 -0400

    add default config

---
+++ config/default.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  queue: {
+       // connection: 'amqp://yedian:yedian123outfox@staging-api.chinacloudapp.cn:5672',
+    connection: 'amqp://user:pwd@127.0.0.1:5672',
+    channel: 'MDC_QUEUE',
+    consumerAdapters: [{
+      queueName: 'email',
+      require: 'mdc-email-smtp'
+    }, {
+      queueName: 'wechat',
+      require: 'mdc-weixin',
+      tokenUrl: 'http://127.0.0.1/getAccessToken'
+    }]
+  },
+  wechatTemplate: {
+    '1': ''
+  }
+}
