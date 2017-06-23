791df2792a3598d3493192ed9c826417b606a535
shadow88sky <119136016@qq.com>
2017-06-23 11:28:54 +0800

    no message

--- common/models/sms.js
+++ common/models/sms.js
@@ -1,57 +1,56 @@
-'use strict';
-var app = require('../../server/server');
+'use strict'
+var app = require('../../server/server')
 module.exports = function (sms) {
-    sms.beforeRemote('create', function (context, user, next) {
-        var req = context.req;
-        req.body.code = MathRand();
-        req.body.createtime = new Date();
-        next();
-    });
-
-    sms.afterRemote('create', function (ctx, modelInstance, next) {
-        let content = `感谢您下载并注册夜点，预订KTV要派对不排队，您的验证码是${modelInstance.code.toString()} 【夜点应用】`;
-        amqpConnection.publish('SmsExchange', 'sms', JSON.stringify({
-            tel: modelInstance.phone,//电话
-            content: content  //模板参数
-        }), 'direct');
-        next();
+  sms.beforeRemote('create', function (context, user, next) {
+      var req = context.req
+        req.body.code = MathRand()
+        req.body.createtime = new Date()
+        next()
     })
 
-    sms.verification = function (req, res) {
-        var smsModel = app.loopback.findModel('sms');
+    sms.afterRemote('create', function (ctx, modelInstance, next) {
+      let content = `感谢您注册Night+夜晚生活玩乐平台，尽享夜晚玩乐资讯&福利，您的验证码是${modelInstance.code.toString()} 【Night+】`
+        amqpConnection.publish('SmsExchange', 'sms', JSON.stringify({
+          tel: modelInstance.phone, // 电话
+          content: content  // 模板参数
+        }), 'direct')
+        next()
+    })
+
+  sms.verification = function (req, res) {
+      var smsModel = app.loopback.findModel('sms')
         smsModel.findOne({ where: { phone: req.body.phone }, order: 'id DESC' }, function (err, sms) {
-            if (err) return res.jsonp({ errcode: 500, errmsg: err });
-            if (!sms) return res.jsonp({ errcode: 403, errmsg: '验证失败' });
+          if (err) return res.jsonp({ errcode: 500, errmsg: err })
+            if (!sms) return res.jsonp({ errcode: 403, errmsg: '验证失败' })
             if (req.body.code == sms.code) {
-                sms.updateAttribute('prooftime', new Date(), function (err, r) {
-                    if (err) return res.jsonp({ errcode: 500, errmsg: err });
-                    res.jsonp({ code: 200, msg: '验证成功' });
+              sms.updateAttribute('prooftime', new Date(), function (err, r) {
+                  if (err) return res.jsonp({ errcode: 500, errmsg: err })
+                    res.jsonp({ code: 200, msg: '验证成功' })
                 })
             } else {
-                res.jsonp({ errcode: 403, errmsg: '验证失败' });
+              res.jsonp({ errcode: 403, errmsg: '验证失败' })
             }
-        });
+        })
     }
-    sms.remoteMethod(
+  sms.remoteMethod(
         'verification',
-        {
-            //accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
-            accepts: [
+      {
+            // accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
+        accepts: [
                 { arg: 'req', type: 'object', 'http': { source: 'req' } },
                 { arg: 'res', type: 'object', 'http': { source: 'res' } }
             ],
-            http: { path: '/verification', verb: 'post' }
-            //returns: { arg: 'result', type: 'object' }
-        }
-    );
+        http: { path: '/verification', verb: 'post' }
+            // returns: { arg: 'result', type: 'object' }
+      }
+    )
 };
 
-function MathRand() {
-    var num = "";
-    for (var i = 0; i < 6; i++) {
-        var random = Math.floor(Math.random() * 10);
-        num += random == 0 ? 1 : random;
+function MathRand () {
+  var num = '';
+  for (var i = 0; i < 6; i++) {
+      var random = Math.floor(Math.random() * 10)
+        num += random == 0 ? 1 : random
     }
-    return num;
+  return num
 }
-
