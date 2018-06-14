db4c8645bbe51a1e3cbf5abfa288a718a80c7a9d
503945930 <503945930@qq.com>
2018-06-14 18:04:41 +0800

    add sms

--- src/consumer/index.ts
+++ src/consumer/index.ts
@@ -1,7 +1,20 @@
 const consumer = require('../lib/rabbitmq/worker')
+import * as config from "config";
+const log4js = require('log4js')
+// const config = require('config')
+
+// log config
+log4js.layouts.addLayout('json', function (config) {
+  return function (logEvent) { return JSON.stringify(logEvent) + config.separator }
+})
 
 
-let retry = 0
+log4js.configure(config.get("log"));
+const logger = log4js.getLogger("consumer");
+
+logger.info('config',config)
+logger.info('start')
+
 consumer.start(function (err:Error) {
   if(err) return  console.log(`rabbitmq error`,err);
   console.log('Consumer started.')
--- src/lib/rabbitmq/worker.js
+++ src/lib/rabbitmq/worker.js
@@ -1,36 +1,61 @@
-var conn = require('./channel')
+const conn = require('./channel')
 const async = require('async')
+
+const log4js = require('log4js')
+const logger = log4js.getLogger('consumer')
+
 
 const CONSUMER_ADAPTERS = require('config').queue.consumerAdapters
 const CHANNEL = require('config').queue.channel
 let ch
 
-exports.start = function (callback) {
+exports.start = function (callback, retry = 0) {
   conn.createChannel().then((channel) => {
     ch = channel
-    ch.assertExchange(`mdc`, 'direct', { durable: true, autoDelete: false })
-    async.eachLimit(CONSUMER_ADAPTERS, 10, function (adapter, callback) {
-      ch.assertQueue(`${CHANNEL}.${adapter.queueName}`, { durable: true, autoDelete: false })
+    ch.assertExchange(`mdc`, 'direct', {
+      durable: true,
+      autoDelete: false
+    })
+    async.eachLimit(CONSUMER_ADAPTERS, 10, function (adapter, cb) {
+      ch.assertQueue(`${CHANNEL}.${adapter.queueName}`, {
+        durable: true,
+        autoDelete: false
+      })
       ch.bindQueue(`${CHANNEL}.${adapter.queueName}`, `mdc`, `${CHANNEL}.${adapter.queueName}`)
       ch.consume(`${CHANNEL}.${adapter.queueName}`, (msg) => {
+        // ch.ack(msg)
         if (msg !== null) {
-          let message = JSON.parse(msg.content.toString()) || {}
-          console.log('message', message)
-          // ch.ack(msg)
-          require(adapter.require).create(adapter).emit(message.emit || 'message', message.message, function (err) {
-            if (err) {
-              ch.ack(msg)
-              return new Error(`Failed to comsume message ${msg.content.toString()}`)
+          const message = JSON.parse(msg.content.toString()) || {}
+          // console.log('work:message',message)
+          // if error retry 3/1 second
+          async.retry({
+            times: 3,
+            interval: 1000
+          }, function (cbr) {
+            require(adapter.require).create(adapter).emit(message.emit || 'message', message.message, function (err, result) {
+             
+
+              return cbr(err, result)
+            })
+          }, function (err, result) {
+            if(err){
+             // console.log('comsumer:err',err)
+              logger.info('comsumer:err',err)
             }
+           // console.log('worker:err', err)
+           
             ch.ack(msg)
+           // return cb(err, result)
           })
         }
-      }, { noAck: false })
-      callback()
+      }, {
+        noAck: false
+      })
     }, function () {
       callback()
     })
   }).catch(err => {
+   
     return callback(err)
   })
 }
