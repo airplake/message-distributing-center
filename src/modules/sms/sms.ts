b0a5600405ec3155506a5a48fd722a081c484433
503945930 <503945930@qq.com>
2018-08-20 17:50:28 +0800

    add minutes

--- src/modules/sms/sms.ts
+++ src/modules/sms/sms.ts
@@ -14,6 +14,7 @@ const publisher = require('../../lib/rabbitmq/producer')
 const smsTemplate = require('config').smsTemplate
 import { AliyunSms } from '../../utils'
 const config = require('config')
+import * as moment from 'moment'
 
 
 const logger = log4js.getLogger('sms')
@@ -23,12 +24,31 @@ sms.post('/',
     async (req: Request, res: Response, next: NextFunction) => {
         try {
             logger.info('sms:post:', req.body)
-            let code = Math.floor(100000 + Math.random() * 900000)
-            let result = await Sms.forge({
-                phone: req.body.phone,
-                code: code,
-                createtime: new Date()
-            }).save()
+
+
+            let result = await Sms.query({ where: { 'phone': req.body.phone } }).orderBy('id', 'DESC').fetch()
+            let code = undefined
+            // 
+            if (result) {
+                // console.log('createtime',moment(result.get('createtime')))
+                // console.log('new', moment(new Date()))
+                // console.log(moment(new Date()).diff(moment(result.get('createtime')), 'minutes'))
+                if (moment(new Date()).diff(moment(result.get('createtime')), 'minutes') > 3) {
+                    console.log('3')
+                    code = Math.floor(100000 + Math.random() * 900000)
+                    result = await Sms.forge({
+                        phone: req.body.phone,
+                        code: code,
+                        createtime: new Date()
+                    }).save()
+                }
+                else {
+                    console.log('4')
+                    code = result.get('code')
+                }
+
+            }
+
             let message = {
                 message: {
                     PhoneNumbers: req.body.phone, // 要发送到短信的手机
@@ -42,14 +62,10 @@ sms.post('/',
                 message.message.TemplateCode = smsTemplate[2].TemplateCode
             }
             logger.info('sms:post:message', message)
-            // mqSend(message, require('config').queue.consumerAdapters[2].queueName, function () {
-            //    // res.send(result)
-            // })
 
-            // mqSend(message, require('config').queue.consumerAdapters[2].queueName)
-            
-            await new AliyunSms(config.get('smsAliyun')).sendRegistSms(message.message)
-            
+
+            // await new AliyunSms(config.get('smsAliyun')).sendRegistSms(message.message)
+
             res.send(result)
         } catch (error) {
             error.Phone = req.body.tel
